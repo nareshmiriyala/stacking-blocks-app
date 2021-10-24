fccb7f284d4799f1632a8e8a38182a1bd9445271
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2021-10-24 16:11:58 +1100

    fix: Fixing name

--- src/main/groovy/au/com/anz/test/stackingblocksapp/biz/DefaultBlockCalculatorHelper.java
+++ src/main/groovy/au/com/anz/test/stackingblocksapp/biz/DefaultBlockCalculatorHelper.java
@@ -14,10 +14,10 @@ import org.springframework.stereotype.Component;
 public class DefaultBlockCalculatorHelper implements BlockCalculatorHelper {
   @Override
   public List<Block> getBlocksSortedDescendingByDimension(List<Block> blocks) {
-    return blocks.stream().map(blocksDimensionOrderedByHavingMaxHeight).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+    return blocks.stream().map(blocksDimensionOrderedByMaxHeight).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
   }
 
-  static final Function<Shape, Block> blocksDimensionOrderedByHavingMaxHeight = (shape) -> {
+  static final Function<Shape, Block> blocksDimensionOrderedByMaxHeight = (shape) -> {
     Block block = (Block) shape;
     List<Integer> dimensions = Stream.of(block.getHeight(), block.getLength(), block.getWidth()).sorted().collect(Collectors.toList());
     return Block.of(dimensions.get(0), dimensions.get(1), dimensions.get(2));
